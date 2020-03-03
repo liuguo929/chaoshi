f86a1c7184bb94590c3982d316785f8d3227eb1e
guo <guo@aaa.com>
2020-03-03 23:31:25 +0800

    主分支改动

--- chaoshi_parent/chaoshi_service/chaoshi_service_goods/src/main/java/com/chaoshi/goods/controller/GoodController.java
+++ chaoshi_parent/chaoshi_service/chaoshi_service_goods/src/main/java/com/chaoshi/goods/controller/GoodController.java
@@ -12,6 +12,7 @@ public class GoodController {
     @GetMapping("/list")
     public Result findList(){
         System.out.println("查询商品列表~~~~");
+        System.out.println("这是master主分支~~~~");
         return new Result(true, StatusCode.OK,"查询成功");
     }
 }
